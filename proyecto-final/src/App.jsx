1be659c7a157e2f1e6d922597142565dd11009bf
kevin <contactohuayta@gmail.com>
2025-11-21 02:54:29 -0300

    termine el diseño de la pagina principal

--- proyecto-final/src/App.jsx
+++ proyecto-final/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header.jsx'
 import Homepage from './components/Homepage/Homepage.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,7 +9,12 @@ import './App.css'
 function App() {
 
   return (
-      <Homepage />
+    <Router>
+      <Homepage/>
+      <Routes>
+        <Route path='/asd' element={<Homepage/>}/>
+      </Routes>
+    </Router>
   )
 }
 
--- proyecto-final/src/components/Homepage/Homepage.jsx
+++ proyecto-final/src/components/Homepage/Homepage.jsx
@@ -1,23 +1,62 @@
-import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
-import {Link } from "react-router-dom";
-import { Hamburger  } from 'lucide-react';
+import React, {useState} from "react";
+import { Container, Row, Col, Button, Nav, Offcanvas } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { Hamburger, TextAlignJustify, X, Facebook, Twitter, Instagram, Youtube, Linkedin } from 'lucide-react';
 
+import Logo from "../../assets/Logo-mostaza.png";
 import styles from "./Homepage.module.css";
 
 function Homepage(){
+    const [showOffcanvas, setShowOffcanvas] = useState(false);
+
+    const handleOpen = () => setShowOffcanvas(true);
+    const handleClose = () => setShowOffcanvas(false);
+
     return(
         <div className={`d-flex align-items-center ${styles.container}`}>
             <Container className="text-center">
-                <Button className={`${styles.btnHamburger }`}>
-                    <Hamburger size={80} strokeWidth={1.3}/>
+                <Button className={`${styles.btnTextAlignJustify} rounded-5`} onClick={handleOpen}>
+                    <TextAlignJustify size={20} strokeWidth={4}/>
                 </Button>
-                <p className="m-3 lh-1 fw-semibold fs-6">
-                    VER
-                    <br/>
-                    MENÚ
-                </p>
+                <div>
+                    <NavLink to="/hamburguer">
+                        <Button className={`${styles.btnHamburger}`}>
+                            <Hamburger size={80} strokeWidth={1.3}/>
+                        </Button>
+                    </NavLink>
+                    <p className="m-3 lh-1 fw-semibold fs-6 text-white">
+                        VER
+                        <br/>
+                        MENÚ
+                    </p>
+                </div>
+                <NavLink to="/">
+                    <img src={Logo} alt="Mostaza" className={`${styles.logo}`}/>
+                </NavLink>
             </Container>
+            <Offcanvas placement="start" show={showOffcanvas} onHide={handleClose} className={`${styles.offcanvas}`}>
+                <Offcanvas.Header className={`${styles.offcanvasHeader} mt-4`}>
+                    <Offcanvas.Title className={`lh-1 fs-2 ms-1`}>Mostaza</Offcanvas.Title>
+                    <Button onClick={handleClose} className={`${styles.btnClose}`}>
+                        <X size={18} strokeWidth={3}/>
+                    </Button>
+                </Offcanvas.Header>
+                <Offcanvas.Body className={`${styles.offcanvasBody}`}>
+                    <Nav className={`${styles.nav1} flex-column w-100 fw-bold`}>
+                        <Nav.Link as={NavLink} to="/Iniciar Sesión">Iniciar Sesión</Nav.Link>
+                        <Nav.Link as={NavLink} to="/Menú">Menú</Nav.Link>
+                        <Nav.Link as={NavLink} to="/Sucursales">Sucursales</Nav.Link>
+                        <Nav.Link as={NavLink} to="/Contacto">Contacto</Nav.Link>
+                    </Nav>
+                    <Nav className={`${styles.nav2}`}>
+                        <Nav.Link as={NavLink} to="https://www.facebook.com/MostazaOk" target="_blank"><Facebook size={25}/></Nav.Link>
+                        <Nav.Link as={NavLink} to="https://x.com/Mostaza_ok" target="_blank"><Twitter size={25}/></Nav.Link>
+                        <Nav.Link as={NavLink} to="https://www.instagram.com/mostazaok" target="_blank"><Instagram size={25}/></Nav.Link>
+                        <Nav.Link as={NavLink} to="https://www.youtube.com/@MostazaOk" target="_blank"><Youtube size={25}/></Nav.Link>
+                        <Nav.Link as={NavLink} to="https://www.linkedin.com/company/mostazaok" target="_blank"><Linkedin size={25}/></Nav.Link>
+                    </Nav>
+                </Offcanvas.Body>
+            </Offcanvas>
         </div>
     );
 }
